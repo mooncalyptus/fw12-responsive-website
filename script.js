9d5e4f2266b134c68e39244d04d078c03a56c1e3
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-11-16 19:08:38 +0700

    added script page to fetch API

---
+++ script.js
@@ -0,0 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fd88834caamshe37d325a609c567p12be51jsne45f5106b192',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+
+const getData = fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err)); 
