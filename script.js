80b49637a8c1823d033d7e605aec78f3d05f02ec
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-11-17 08:38:36 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fd88834caamshe37d325a609c567p12be51jsne45f5106b192',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'fd88834caamshe37d325a609c567p12be51jsne45f5106b192',
+// 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+// 	}
+// };
 
-const getData = fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err)); 
+// const getData = fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err)); 
+
+//     console.log(getData);
